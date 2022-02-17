03fb410f9cdf77e55bf6e75370145ad54639aecd
shubham <shubham.devops.cloud@gmail.com>
2022-02-17 20:05:24 +0530

    updatedd deploy file

--- deploy.groovy
+++ deploy.groovy
@@ -1,10 +1,24 @@
 pipeline{
   agent any
 
+  options {
+    buildDiscarder logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10')
+  }
+
+  paraeters{
+    choice choices: ['stage', 'promote', 'stage_and_promote', 'deploy_prod'], description: 'build mode', name: 'mode'
+    extendedChoice(description: 'repositories to release', multiSelectDelimiter: ',', name: 'repos',
+                    quoteValue: false, saveJSONParameterToFile: false, type: 'PT_MULTI_SELECT', visibleItemCount: 11,
+                    value: 'udm,util,onboarding,admin,contracts,deliveries,integration,overview,settlements,approuter,ui')
+    string defaultValue: '1.0.0', description: 'version to release', name: 'version'
+  }
+
   stages{
     stage('build'){
       steps{
-        echo "Hello"
+        echo "${params.mode}"
+        echo "${params.repos}"
+        echo "${params.version}"
       }
     }
   }
